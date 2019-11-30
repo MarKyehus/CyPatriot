c6237d583f49fdf557aba1d1a341d6b467dbf618
MarKyehus <47043479+MarKyehus@users.noreply.github.com>
2019-11-30 00:29:00 -0800

    Create install.sh

---
+++ Ubuntu 14.04/script/install.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# MySQL
+echo -n "MySQL [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get -y install mysql-server
+  # Disable remote access
+  sudo sed -i '/bind-address/ c\bind-address = 127.0.0.1' /etc/mysql/my.cnf
+  sudo service mysql restart
+else
+  sudo apt-get -y purge mysql*
+fi
+
+# OpenSSH Server
+echo -n "OpenSSH Server [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get -y install openssh-server
+  # Disable root login
+  sudo sed -i '/^PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
+  sudo service ssh restart
+else
+  sudo apt-get -y purge openssh-server*
+fi
+
+# VSFTPD
+echo -n "VSFTP [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get -y install vsftpd
+  # Disable anonymous uploads
+  sudo sed -i '/^anon_upload_enable/ c\anon_upload_enable no' /etc/vsftpd.conf
+  sudo sed -i '/^anonymous_enable/ c\anonymous_enable=NO' /etc/vsftpd.conf
+  # FTP user directories use chroot
+  sudo sed -i '/^chroot_local_user/ c\chroot_local_user=YES' /etc/vsftpd.conf
+  sudo service vsftpd restart
+else
+  sudo apt-get -y purge vsftpd*
+fi
+
+done
