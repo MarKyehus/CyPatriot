f9f3c9b8c2a444c60ee95d083cb1fe09373be721
Mark <47043479+MarKyehus@users.noreply.github.com>
2019-12-16 09:32:28 -0800

    Add files via upload

---
+++ Linux/Ubuntu 14.04/script/script/install.sh
@@ -0,0 +1,110 @@
+#! /bin/bash
+
+# MySQL
+echo -n "MySQL [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get purge mysql-server
+  sudo apt-get -y install mysql-server
+  # Disable remote access
+  sudo sed -i '/bind-address/ c\bind-address = 127.0.0.1' /etc/mysql/my.cnf
+  sudo service mysql restart
+   echo "$(date +'%m/%d/%Y %r'): Installed MySQL" >> ./log/oats.txt
+else
+  sudo apt-get -y purge mysql*
+fi
+
+# OpenSSH Server
+echo -n "OpenSSH Server [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get purge openssh-server
+  sudo apt-get -y install openssh-server
+  # Disable root login
+  sudo sed -i '/^PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
+  sudo service ssh restart
+  echo "$(date +'%m/%d/%Y %r'): Installed OpenSSH Server" >> ./log/oats.txt
+else
+  sudo apt-get -y purge openssh-server*
+fi
+
+# VSFTPD
+echo -n "VSFTP [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get purge vsftpd
+  sudo apt-get -y install vsftpd
+  # Disable anonymous uploads
+  sudo sed -i '/^anon_upload_enable/ c\anon_upload_enable no' /etc/vsftpd.conf
+  sudo sed -i '/^anonymous_enable/ c\anonymous_enable=NO' /etc/vsftpd.conf
+  # FTP user directories use chroot
+  sudo sed -i '/^chroot_local_user/ c\chroot_local_user=YES' /etc/vsftpd.conf
+  sudo service vsftpd restart
+  echo "$(date +'%m/%d/%Y %r'): Installed VSFTPD" >> ./log/oats.txt
+else
+  sudo apt-get -y purge vsftpd*
+fi
+
+# AntiVirus
+echo -n "AntiVirus [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo apt-get install chkrootkit rkhunter lynis clamav
+   echo "$(date +'%m/%d/%Y %r'): Installed chkrootkit rkhunter lynis clamav" >> ./log/oats.txt
+  #chrootkit 
+    echo "starting chkrootkit scan"
+      chkrootkit -q
+	  cont
+  #rkhunter
+  echo "starting rkhunter scan"
+      rkhunter --update
+      rkhunter --propupd #Run this once at install
+      rkhunter -c --enable all --disable none
+	  cont
+  #lynis
+    echo "starting lynis scan"
+      cd /usr/share/lynis/
+      /usr/share/lynis/lynis update info
+      /usr/share/lynis/lynis audit system
+    cont
+  #clamav
+    echo "starting clamav scan"
+      systemctl stop clamav-freshclam
+      freshclam --stdout
+      systemctl start clamav-freshclam
+      clamscan -r -i --stdout --exclude-dir="^/sys" /
+    cont
+else
+  sudo apt-get -y purge chkrootkit*
+  sudo apt-get -y purge rkhunter*
+  sudo apt-get -y purge lynis*
+  sudo apt-get -y purge clamav*
+fi
+
+# Gnome Terminal
+echo -n "Gnome Emulator [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo add-apt-repository ppa:gnome3-team/gnome3
+  sudo apt-get update && sudo apt-get install gnome-shell ubuntu-gnome-desktop
+  echo "$(date +'%m/%d/%Y %r'): Installed gnome" >> ./log/oats.txt
+else
+  sudo apt-get -y purge gnome-shell ubuntu-gnome-desktop*
+fi
+
+# Terminator
+echo -n "Terminator Emulator [Y/n] "
+read option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  sudo add-apt-repository ppa:gnome-terminator
+  sudo apt-get update && sudo apt-get install terminator
+  echo "$(date +'%m/%d/%Y %r'): Installed terminator" >> ./log/oats.txt
+else
+  sudo apt-get remove --auto-remove terminator
+fi
